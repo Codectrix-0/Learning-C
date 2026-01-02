31b073c1e4bef1160042158f40454cd88066d81f
Divya Mer <divyamer066@gmail.com>
2026-01-02 15:17:03 +0530

    Update swaping.c

--- swaping.c
+++ swaping.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int i;
-	for (i=0;i<5;i++)
-	printf("\n Hello");
-	return 0;
+    int a,b,temp;
+    printf("enter frist number");
+    scanf("%d,&a");
+    printf("enter second number");
+    scanf("%d",&b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("\nvalue of a is %d",a);
+    printf("\nvalue of b is %d",b);
+    return 0;
 }
